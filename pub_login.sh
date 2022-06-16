0a092a0ad2f3cd9cffbc04259a2c176408c9ace1
lozn <91799281+lozn@users.noreply.github.com>
2022-06-16 11:13:42 +0800

    fix script

--- pub_login.sh
+++ pub_login.sh
@@ -5,27 +5,27 @@
 # variables or not.
 if [ -z "${PUB_DEV_PUBLISH_ACCESS_TOKEN}" ]; then
   echo "Missing PUB_DEV_PUBLISH_ACCESS_TOKEN environment variable"
-  exit 1
+#   exit 1
 fi
 
 if [ -z "${PUB_DEV_PUBLISH_REFRESH_TOKEN}" ]; then
   echo "Missing PUB_DEV_PUBLISH_REFRESH_TOKEN environment variable"
-  exit 1
+#   exit 1
 fi
 
 if [ -z "${PUB_DEV_PUBLISH_ID_TOKEN}" ]; then
   echo "Missing PUB_DEV_PUBLISH_ID_TOKEN environment variable"
-  exit 1
+#   exit 1
 fi
 
 if [ -z "${PUB_DEV_PUBLISH_TOKEN_ENDPOINT}" ]; then
   echo "Missing PUB_DEV_PUBLISH_TOKEN_ENDPOINT environment variable"
-  exit 1
+#   exit 1
 fi
 
 if [ -z "${PUB_DEV_PUBLISH_EXPIRATION}" ]; then
   echo "Missing PUB_DEV_PUBLISH_EXPIRATION environment variable"
-  exit 1
+#   exit 1
 fi
 
 # Create pub-credentials.json file.
